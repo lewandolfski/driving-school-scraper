5ce1e85485a11abb57e42f810a549f7967430347
lewandolfski <dolfdelft@gmail.com>
2025-09-14 15:06:17 +0200

    Fix httpx import error by removing example_scraper from __init__.py
    - Removed import of ExampleDrivingSchoolScraper that requires httpx
    - Only import essential base classes needed for incremental scraper
    - This fixes the ModuleNotFoundError preventing scraper startup

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,6 +1,5 @@
 """Driving school scrapers package."""
 
 from .base_scraper import ScrapedSchool, BaseScraper
-from .example_scraper import ExampleDrivingSchoolScraper
 
-__all__ = ['ScrapedSchool', 'BaseScraper', 'ExampleDrivingSchoolScraper']
+__all__ = ['ScrapedSchool', 'BaseScraper']
